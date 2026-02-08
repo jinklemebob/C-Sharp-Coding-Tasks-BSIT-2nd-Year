cbc9d9e408a6e165930b4888415b83adfa49f6a1
Jinklemebob <proximdrag@gmail.com>
2026-02-08 19:41:45 +0800

    Added 03 Task Performance 1

---
+++ PRELIMS ( .NET Framework, Data Types, Operators and Expressions)/03 Task Performance 1/ComputeAverageApp/ComputeAverageProgram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeAverageApp
+{
+    internal class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("<<< COMPUTE AVERAGE APP >>>");
+            Console.WriteLine("Enter 5 Numeric Grades to Compute The Average (New line for each) ");
+            Console.Write("First: ");
+            double first = Convert.ToDouble(Console.ReadLine());   
+            Console.Write("Second: ");
+            double second = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Third: ");
+            double third = Convert.ToDouble(Console.ReadLine());    
+            Console.Write("Fourth: ");
+            double fourth = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Fifth: ");
+            double fifth = Convert.ToDouble(Console.ReadLine());  
+            
+            double averages = (first + second + third + fourth + fifth)/5;
+            double rounded = Math.Round(averages);
+
+            Console.WriteLine("\n<<< RESULTS >>>");
+            Console.WriteLine("The total average is " + "'" + averages + "'" + " and rounded off to " + "'" + rounded + "'" + ".");
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
